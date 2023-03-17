266a9420ec8d6c84edc2e230262b1745ce513e99
Adrian Kunz <clashsoft@hotmail.com>
2023-03-17 11:57:35 +0100

    feat: Use Events in example

--- apps/example/src/app/app.module.ts
+++ apps/example/src/app/app.module.ts
@@ -1,11 +1,21 @@
-import { Module } from '@nestjs/common';
+import {EventModule, USER_ID_PROVIDER} from '@clashsoft/nestx';
+import {Module} from '@nestjs/common';
+import {Transport} from '@nestjs/microservices';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    EventModule.register(Transport.NATS, {
+      servers: 'nats://localhost:4222',
+    }, {
+      provide: USER_ID_PROVIDER,
+      useValue: async () => '123',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
--- apps/example/src/main.ts
+++ apps/example/src/main.ts
@@ -3,19 +3,29 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import {initEventGateway} from '@clashsoft/nestx';
+import {Logger} from '@nestjs/common';
+import {NestFactory} from '@nestjs/core';
+import {Transport} from '@nestjs/microservices';
 
-import { AppModule } from './app/app.module';
+import {AppModule} from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  initEventGateway('/ws/events');
+  app.connectMicroservice({
+    transport: Transport.NATS,
+    options: {
+      servers: 'nats://localhost:4222',
+    },
+  });
+  await app.startAllMicroservices();
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
   );
 }
 
