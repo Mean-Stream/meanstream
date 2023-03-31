be997cbf50d88cd4537f1eebc68cb01ee6b8fbc1
Adrian Kunz <a.kunz@uni-kassel.de>
2023-03-31 14:37:07 +0200

    fix: Include Event module definition in index

--- libs/nestx/src/lib/event/index.ts
+++ libs/nestx/src/lib/event/index.ts
@@ -1,3 +1,4 @@
-export * from './event.gateway';
-export * from './event.service';
+export * from './event.module-def';
 export * from './event.module';
+export * from './event.service';
+export * from './event.gateway';
