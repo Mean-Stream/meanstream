50e1fc5fe0ecee010dab20cd31e6abcd29b7dc5c
Adrian Kunz <a.kunz@uni-kassel.de>
2023-04-13 10:34:45 +0200

    feat(example-web): Add example-web project

---
+++ apps/example-web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nestx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'example-web';
+}
