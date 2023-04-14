c096671e02787e32f17bccdd9df686312e2bf1d1
Adrian Kunz <clashsoft@hotmail.com>
2023-04-14 17:44:19 +0200

    refactor(nestx): Implement OptionalRef and RefArray as options in Ref

--- libs/nestx/src/lib/ref/ref.decorator.ts
+++ libs/nestx/src/lib/ref/ref.decorator.ts
@@ -14,39 +14,39 @@ function objectId<T extends string | number | Types.ObjectId | Uint8Array>(value
 
 const EXAMPLE_OBJECT_ID = '62fc9b33773277d12d28929b';
 
-export function Ref(ref: string): PropertyDecorator {
+export function Ref(ref: string, {array, optional}: {
+  array?: boolean;
+  optional?: boolean;
+} = {}): PropertyDecorator {
   const decorators: PropertyDecorator[] = [
-    Transform(({value}) => objectId(value)),
-    IsInstance(Types.ObjectId),
+    Transform(array ? ({value}) => Array.isArray(value) && value.map(objectId) : ({value}) => objectId(value)),
+    IsInstance(Types.ObjectId, {each: array}),
   ];
+
+  if (optional) {
+    decorators.push(IsOptional());
+  }
+
   const mongoose = optionalRequire('@nestjs/mongoose');
-  mongoose && decorators.push(mongoose.Prop({type: Types.ObjectId, ref, required: true}));
+  mongoose && decorators.push(mongoose.Prop({
+    type: array ? [{type: Types.ObjectId, ref}] : Types.ObjectId,
+    ref: array ? undefined : ref,
+    required: !optional,
+  }));
   const swagger = optionalRequire('@nestjs/swagger');
-  swagger && decorators.push(swagger.ApiProperty({example: EXAMPLE_OBJECT_ID, format: 'objectid'}));
+  swagger && decorators.push(swagger.ApiProperty({
+    isArray: array,
+    example: array ? [EXAMPLE_OBJECT_ID] : EXAMPLE_OBJECT_ID,
+    format: 'objectid',
+    required: !optional,
+  }));
   return applyDecorators(...decorators);
 }
 
 export function OptionalRef(ref: string): PropertyDecorator {
-  const decorators: PropertyDecorator[] = [
-    Transform(({value}) => objectId(value)),
-    IsOptional(),
-    IsInstance(Types.ObjectId),
-  ];
-  const mongoose = optionalRequire('@nestjs/mongoose');
-  mongoose && decorators.push(mongoose.Prop({type: Types.ObjectId, ref}));
-  const swagger = optionalRequire('@nestjs/swagger');
-  swagger && decorators.push(swagger.ApiPropertyOptional({example: EXAMPLE_OBJECT_ID, format: 'objectid'}));
-  return applyDecorators(...decorators);
+  return Ref(ref, {optional: true});
 }
 
 export function RefArray(ref: string) {
-  const decorators: PropertyDecorator[] = [
-    Transform(({value}) => Array.isArray(value) && value.map(objectId)),
-    IsInstance(Types.ObjectId, {each: true}),
-  ];
-  const mongoose = optionalRequire('@nestjs/mongoose');
-  mongoose && decorators.push(mongoose.Prop({type: [{type: Types.ObjectId, ref}]}));
-  const swagger = optionalRequire('@nestjs/swagger');
-  swagger && decorators.push(swagger.ApiProperty({isArray: true, example: [EXAMPLE_OBJECT_ID], format: 'objectid'}));
-  return applyDecorators(...decorators);
+  return Ref(ref, {array: true});
 }
