0d1935ffa909b78a076148383c85fc3555023758
Adrian Kunz <a.kunz@uni-kassel.de>
2023-05-23 15:56:56 +0200

    feat(nestx): Add index from Ref decorator

--- libs/nestx/src/lib/ref/ref.decorator.ts
+++ libs/nestx/src/lib/ref/ref.decorator.ts
@@ -2,15 +2,19 @@ import {applyDecorators} from '@nestjs/common';
 import {optionalRequire} from '@nestjs/core/helpers/optional-require';
 import {Transform} from 'class-transformer';
 import {IsInstance, IsOptional} from 'class-validator';
-import {Types} from 'mongoose';
+import {IndexDefinition, IndexDirection, Types} from 'mongoose';
 import {objectId} from './objectid.helper';
+import type {PropOptions} from '@nestjs/mongoose';
 
 const EXAMPLE_OBJECT_ID = '62fc9b33773277d12d28929b';
 
-export function Ref(ref: string, {array, optional}: {
+export type RefOptions = {
   array?: boolean;
   optional?: boolean;
-} = {}): PropertyDecorator {
+  index?: boolean | IndexDirection | IndexDefinition;
+};
+
+export function Ref(ref: string, {array, optional, index}: RefOptions = {}): PropertyDecorator {
   const decorators: PropertyDecorator[] = [
     Transform(array ? ({value}) => Array.isArray(value) && value.map(objectId) : ({value}) => objectId(value)),
     IsInstance(Types.ObjectId, {each: array}),
@@ -21,14 +25,20 @@ export function Ref(ref: string, {array, optional}: {
   }
 
   const mongoose = optionalRequire('@nestjs/mongoose');
-  mongoose && decorators.push(mongoose.Prop(array ? {
-    type: [{type: Types.ObjectId, ref}],
-    required: !optional,
-  } : {
-    type: Types.ObjectId,
-    ref,
-    required: !optional,
-  }));
+  if (mongoose) {
+    const propOptions: PropOptions = array ? {
+      type: [{type: Types.ObjectId, ref}],
+      required: !optional,
+    } : {
+      type: Types.ObjectId,
+      ref,
+      required: !optional,
+    };
+    if (index) {
+      propOptions.index = index;
+    }
+    decorators.push(mongoose.Prop(propOptions));
+  }
   const swagger = optionalRequire('@nestjs/swagger');
   swagger && decorators.push(swagger.ApiProperty({
     isArray: array,
