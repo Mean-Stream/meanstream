1b72ee82cac29ebd7bcfa1334418bf94d3db6065
Adrian Kunz <a.kunz@uni-kassel.de>
2023-06-03 16:38:15 +0200

    feat(example-service): Use EventRepository

--- apps/example-service/src/user/user.schema.ts
+++ apps/example-service/src/user/user.schema.ts
@@ -1,7 +1,10 @@
 import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
+import {Types} from "mongoose";
 
 @Schema()
 export class User {
+  _id: Types.ObjectId;
+
   @Prop()
   name: string;
 }
--- apps/example-service/src/user/user.service.ts
+++ apps/example-service/src/user/user.service.ts
@@ -1,13 +1,20 @@
 import {Injectable} from '@nestjs/common';
-import {MongooseRepository} from "@clashsoft/nestx";
+import {EventService, MongooseRepository} from "@clashsoft/nestx";
 import {User} from "./user.schema";
 import {InjectModel} from "@nestjs/mongoose";
+import {EventRepository} from "@clashsoft/nestx";
 
 @Injectable()
+@EventRepository()
 export class UserService extends MongooseRepository<User> {
   constructor(
     @InjectModel(User.name) model,
+    private eventService: EventService,
   ) {
     super(model);
+  }
+
+  emit(event: string, data: User) {
+    this.eventService.emit(`users.${data._id}.${event}`, data);
   }
 }
