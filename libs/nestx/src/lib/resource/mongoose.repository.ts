d3461f2357b678bd52a3117c1831159820ce72fe
Adrian Kunz <a.kunz@uni-kassel.de>
2023-06-03 14:21:25 +0200

    feat(nestx): Add Repository and MongooseRepository

---
+++ libs/nestx/src/lib/resource/mongoose.repository.ts
@@ -0,0 +1,56 @@
+import {FilterQuery, Model, QueryOptions, Types, UpdateQuery, UpdateWriteOpResult} from "mongoose";
+import {Doc} from "../ref";
+import {Repository} from "./repository";
+
+
+export class MongooseRepository<T, ID = Types.ObjectId, DOC = Doc<T, ID>>
+  implements Repository<T, ID, DOC, FilterQuery<T>, UpdateQuery<T>> {
+  constructor(
+    protected readonly model: Model<T, object, object, object, DOC>,
+  ) {
+  }
+
+  async create(dto: Omit<T, '_id' | 'createdAt' | 'updatedAt'>): Promise<DOC> {
+    return this.model.create(dto);
+  }
+
+  async findOne(id: ID, options?: QueryOptions<T>): Promise<DOC | null> {
+    return this.model.findById(id, undefined, options).exec();
+  }
+
+  async findAll(filter: FilterQuery<T>, options?: QueryOptions<T>): Promise<DOC[]> {
+    return this.model.find(filter, undefined, options).exec();
+  }
+
+  async update(id: ID, update: UpdateQuery<T>, options: QueryOptions<T> = {}): Promise<DOC | null> {
+    return this.model.findByIdAndUpdate(id, update, {...options, new: true}).setOptions(options).exec();
+  }
+
+  async upsert(filter: FilterQuery<T>, update: UpdateQuery<T>, options: QueryOptions<T> = {}): Promise<DOC | null> {
+    return this.model.findOneAndUpdate(filter, update, {...options, new: true, upsert: true}).exec();
+  }
+
+  async upsertRaw(filter: FilterQuery<T>, update: UpdateQuery<T>, options: QueryOptions<T> = {}): Promise<{ operation: 'created' | 'updated', result: DOC | null }> {
+    const result = await this.model.findOneAndUpdate(filter, update, {...options, new: true, upsert: true, rawResult: true}).exec();
+    if (result.lastErrorObject.upserted) {
+      return { operation: 'created', result: result.value };
+    } else {
+      return { operation: 'updated', result: result.value };
+    }
+  }
+
+  async updateMany(filter: FilterQuery<T>, update: UpdateQuery<T>, options: QueryOptions<T> = {}): Promise<UpdateWriteOpResult> {
+    return this.model.updateMany(filter, update, {...options, new: true}).exec();
+  }
+
+  async delete(id: ID, options?: QueryOptions<T>): Promise<DOC | null> {
+    return this.model.findByIdAndDelete(id, options);
+  }
+
+  async deleteMany(filter: FilterQuery<T>, options?: QueryOptions<T>): Promise<{
+    acknowledged: boolean;
+    deletedCount: number
+  }> {
+    return this.model.deleteMany(filter, options).exec();
+  }
+}
---
+++ libs/nestx/src/lib/resource/repository.ts
@@ -0,0 +1,36 @@
+export interface UpdateManyResult {
+  acknowledged: boolean;
+  matchedCount: number;
+  modifiedCount: number;
+  upsertedCount: number;
+}
+
+export interface RawUpsertResult<DOC> {
+  operation: 'created' | 'updated',
+  result: DOC | null;
+}
+
+interface DeleteManyResult {
+  acknowledged: boolean;
+  deletedCount: number;
+}
+
+export interface Repository<T, ID, DOC, FILTER, UPDATE> {
+  create(dto: Omit<T, '_id' | 'createdAt' | 'updatedAt'>): Promise<DOC>;
+
+  findOne(id: ID): Promise<DOC | null>;
+
+  findAll(filter: FILTER): Promise<DOC[]>;
+
+  update(id: ID, update: UPDATE): Promise<DOC | null>;
+
+  upsert(filter: FILTER, update: UPDATE): Promise<DOC | null>;
+
+  upsertRaw(filter: FILTER, update: UPDATE): Promise<RawUpsertResult<DOC>>;
+
+  updateMany(filter: FILTER, update: UPDATE): Promise<UpdateManyResult>;
+
+  delete(id: ID): Promise<DOC | null>;
+
+  deleteMany(filter: FILTER): Promise<DeleteManyResult>;
+}
