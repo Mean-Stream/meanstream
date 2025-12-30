8f90595b125f20cbc093cd367fe878004d0b494c
Adrian Kunz <a.kunz@uni-kassel.de>
2025-12-30 22:33:19 +0100

    feat(nestx): Better DTO accuracy
    - Support for UUID, Binary, Buffer, BSONSymbol
    - Special support for Decimal128 and Timestamp
    - Custom tagging with {toJSON(): string}

--- libs/nestx/src/lib/ref/dto.ts
+++ libs/nestx/src/lib/ref/dto.ts
@@ -1,4 +1,5 @@
-import type {Types, Document} from 'mongoose';
+import type {Binary, Decimal128, ObjectId, Timestamp, UUID, BSONSymbol} from 'bson';
+import type {Document, Types} from 'mongoose';
 
 type Expand<T> = T extends (...args: infer A) => infer R
   ? (...args: Expand<A>) => Expand<R>
@@ -6,13 +7,26 @@ type Expand<T> = T extends (...args: infer A) => infer R
     ? { [K in keyof O]: O[K] }
     : never;
 
+export type DTOBecomesDecimal128 =
+  | Decimal128 | Types.Decimal128;
+export type DTOBecomesTimestamp =
+  | Timestamp;
+export type DTOBecomesString =
+  | ObjectId | Types.ObjectId
+  | Binary | Types.Buffer
+  | UUID | Types.UUID // subclasses of Binary, but included if that changes.
+  | Date
+  | BSONSymbol
+  | {toJSON(): string}
+  ;
+
 export type DTO<T> =
-  T extends Types.ObjectId ? string :
-    T extends Date ? string :
-      T extends Array<infer U> ? Array<DTO<U>> :
-        T extends object ?
-          Expand<{
-            [P in keyof T]: DTO<T[P]>;
-          }> : T;
+  T extends DTOBecomesString ? string
+    : T extends DTOBecomesDecimal128 ? { $numberDecimal: string }
+      : T extends DTOBecomesTimestamp ? { $timestamp: string }
+        : T extends Document<infer U> ? DTO<U>
+          : T extends Array<infer U> ? Array<DTO<U>>
+            : T extends object ? Expand<{ [P in keyof T]: DTO<T[P]> }>
+              : T;
 
 export type Doc<T, ID = Types.ObjectId, HELP = object> = T & Document<ID, HELP, T>;
