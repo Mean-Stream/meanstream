301ece9b26a64999f8150248c64bf233ee7d842c
Adrian Kunz <a.kunz@uni-kassel.de>
2025-12-31 11:06:36 +0100

    chore: Migrate to Nx 22.3

--- apps/example-web/src/main.ts
+++ apps/example-web/src/main.ts
@@ -1,8 +1,8 @@
 /// <reference types="@angular/localize" />
-
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import {provideZoneChangeDetection} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {AppModule} from './app/app.module';
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+  .bootstrapModule(AppModule, {applicationProviders: [provideZoneChangeDetection()]})
   .catch((err) => console.error(err));
